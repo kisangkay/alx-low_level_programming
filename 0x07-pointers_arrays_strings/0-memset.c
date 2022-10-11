beaf0d2e6cc50b3bdf74bb6d28cc549836343b75
Stephen <kaykisang@gmail.com>
2022-10-11 03:25:42 -0700

    Memory set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
- * declare the local variables
- * @s: is the pointer to put the constant
- * @b: is the constant
- * @n: is the number of byte
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
  * Return: s
  */
+
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-
-	for (i = 0; n > 0; i++; n--)
+	for (i = 0; n > 0; i++, n--)
 	{
 		s[i] = b;
 	}
 
 	return (s);
-
 }
